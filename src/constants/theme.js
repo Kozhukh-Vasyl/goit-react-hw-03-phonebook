07667fa5177d5003c922ee660399651d22391298
Kozhukh-Vasyl <v.kozhukh@gmail.com>
2023-05-03 21:09:34 +0200

    add files

---
+++ src/constants/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    text: '#40916c',
+    textAccent: '#40916c',
+    textError: '#40916c',
+    bgd: '#f6fff8',
+    bgdAccent: '#d2d2d2',
+    border: '#d2d2d2',
+    borderHover: '#40916c',
+    btn: '#c7f9cc',
+    btnHover: '#f6fff8',
+  },
+  spacing: value => `${4 * value}px`,
+};
